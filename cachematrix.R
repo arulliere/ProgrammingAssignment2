0e3aacbcbb304721ca0e532a8fcaa18272d0a82e
Antoine Rulliere <arulliere@gmail.com>
2015-06-12 08:45:56 -0400

    Comments updates
    Added more comments to make code clearer

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,19 @@
 
 
 ## The objective is to create a special "matrix" object that can cache its inverse
+## Use makeCacheMatrix to instanciate a matrix:
+## For instance this sets a 3x3 indentity matrix:
+## mat <- makeCacheMatrix(matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 1), nrow = 3, ncol = 3, byrow = TRUE, dimnames = list(c("row1", "row2", "row3"), c("col1", "col2", "col3"))))
+## Call the inverse matrix using cacheSolve(mat), which will return the inverse from the cache if it has been cached previously
 
 makeCacheMatrix <- function(x = matrix()) {
         ## Instantiate to NULL the variable that will receive the matrix's inverse
         minv <- NULL
         
-        ## Define the set function to cache values
+        ## Define the set function to cache values by substitution if a new value is set
         set <- function(y) {
                 x <<- y
+                ## R-initialize minv to NULL
                 minv <<- NULL
         }
         
@@ -32,7 +37,10 @@ makeCacheMatrix <- function(x = matrix()) {
                 minv
         }
         
-        ##
+        ## This list stores the 4 functions that have been defined in the main function
+        ## Those functions can be accessed by:
+        ## Creating a matrix: mat <- makeCacheMatrix(matrix(c(), nrow = , ncol = , ...))
+        ## calling the object follwed by $function_name(): mat$get()
         list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -47,9 +55,12 @@ cacheSolve <- function(x, ...) {
         
         ## check if matrix's inverse exists in cache
         if (!is.null(minv)){
+                message("Returning cached data")
                 return(minv) 
         }
         
+        ## else
+        message("First time calculating this inverse, the result was not in the cache")
         data <- x$get()
         
         ##inverse the matrix
